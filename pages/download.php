3e758f80e20adb6a07ff89891064d0bfa15fde8b
Gabriel Danilo do Prado <gaa.prado@hotmail.com>
2019-11-23 17:38:37 -0200

    Updating ulrs

--- pages/download.php
+++ pages/download.php
@@ -10,11 +10,21 @@ $main_content .= '<table style="width:100%;margin-left:auto;margin-right:auto">
 //region Client Link
 $main_content .= '<tr bgcolor="' . $config['site']['lightborder'] . '">
                   <td style="font-weight:bold;width:150px">Client próprio</td>
+                  </tr>
+                  <tr bgcolor="' . $config['site']['darkborder'] . '">
                   <td><a href=' . $config['site']['client'] . '>Link</a></td>
                   <td style="font-weight:bold;width:150px">Ucrtbased.dll</td>
+                  </tr>
+                  <tr bgcolor="' . $config['site']['lightborder'] . '">
                   <td><a href=' . $config['site']['ucrtbased'] . '>Link</a></td>
+                  </tr>
+                  <tr bgcolor="' . $config['site']['darkborder'] . '">
                   <td style="font-weight:bold;width:150px">Vcruntime.dll</td>
                   <td><a href=' . $config['site']['vcruntime'] . '>Link</a></td>
+                  </tr>
+                  <tr bgcolor="' . $config['site']['lightborder'] . '">
+                  <td style="font-weight:bold;width:150px">Msvcp140d.dll</td>
+                  <td><a href=' . $config['site']['msvcp140d'] . '>Link</a></td>
                   </tr>';
 //endregion
 
